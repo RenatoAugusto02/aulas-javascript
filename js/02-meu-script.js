9713b715a115f0bb6e84de9cb3e8985f9fb05d4b
Renato Augusto Souza Novais <renatoaugustopl06@gmail.com>
2025-07-30 16:53:18 -0300

    js

---
+++ js/02-meu-script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let teste = "senac";
+console.log(teste);
+
+let tela = "Etc e tal";
+console.log(tela);
+
+/*Criando uma variavel e atribuindo um valor*/
+let uc, Comp;
+uc = 2;
+Comp = "Programação Web com JavaScript e CSS";
+
+/*Exibindo o valor da variavel*/
+console.log(`Estamos na UC${uc} - ${Comp}`);
+
+//console.log("Gosto "muito" de paçoca!"); ERRADO!!!
+
+//CORRETO!   Aspas simples como comando, aspas duplas como texto
+console.log('Gosto "muito" de paçoca!');
+
+//CORRETO!   Aspas duplas como comando, aspas simples como texto
+console.log("Gosto 'muito' de paçoca!");
+
+//CORRETO!   Escapando as aspas como texto
+console.log('Gosto \"muito\" de paçoca!');
