d4049e573fbbbb39ebf8fb28c55d9eb9a5a51ada
Renato Augusto Souza Novais <renatoaugustopl06@gmail.com>
2025-08-06 15:12:02 -0300

    Exercicios JS

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -14,3 +14,13 @@ const coisas = [10, "Senac", "<h2>Oie</h2>", seila, 15.88];
 console.log(coisas[1]);
 console.log(coisas [4]);
 
+/*Exercicios*/
+const atividade = ["Pericles", "Kleberiano", "Zezão", "GTA5", "Futebol", "Fifa 20", "Pes 18", "Sambinha"];
+
+
+console.log(atividade[1]);
+console.log(atividade[4]);
+console.log(atividade[7]);
+
+console.log(`Gosto de ouvir as músicas do ${atividade[1]} e do ${atividade[2]}`);
+
