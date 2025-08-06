e8cdae97c5f58d09b4bb8e1a38cda8911a07275a
Renato Augusto Souza Novais <renatoaugustopl06@gmail.com>
2025-08-06 13:40:28 -0300

    Aulas JS

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -1,2 +1,9 @@
 'use strict';
 
+//Declarar um array
+let vocalista = ["Manoel Gomes", "Kleberiano", "Rogerio SkayLab", "Ozzy", "Dio", "Lemmy", "David Gilmour"]
+
+console.log( vocalistas[0]);
+
+console.log(`O ${vocalistas[3]}`);
+
