61a62445c0e1ca9b1eaa92c252a1348aaeff3bde
Renato Augusto Souza Novais <renatoaugustopl06@gmail.com>
2025-08-06 16:32:09 -0300

    Definindo Objeto

---
+++ js/04_objetos.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Exemplo 1: Objeto com dados de uma pessoa
+const pessoa = {
+    nome: "Chaves",
+    idade: 8,
+    cidade: "São Paulo",
+    estado: "SP",
+};
+
+console.log(pessoa);
+
+// Acessamos as prioridades usando NomeDoObjeto.NomeDaPrioridade
+console.log(`O ${pessoa.nome} tem ${pessoa.idade} anos de idade`);
