62943456acb03f998374a028caa9235239ba3b6a
Renato Augusto Souza Novais <renatoaugustopl06@gmail.com>
2025-08-06 14:34:01 -0300

    Eterogênio arrays

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -1,9 +1,16 @@
 'use strict';
 
 //Declarar um array
-let vocalista = ["Manoel Gomes", "Kleberiano", "Rogerio SkayLab", "Ozzy", "Dio", "Lemmy", "David Gilmour"]
+let vocalistas = ["Manoel Gomes", "Kleberiano", "Rogerio SkayLab", "Ozzy", "Dio", "Lemmy", "David Gilmour"]
 
-console.log( vocalistas[0]);
+console.log(vocalistas[1]);
 
 console.log(`O ${vocalistas[3]}`);
+
+// Relembrando como criar/usar arrays(vator) 
+// No JS, você pode colocar qualquer coisa em array
+let  seila = 6;
+const coisas = [10, "Senac", "<h2>Oie</h2>", seila, 15.88];
+console.log(coisas[1]);
+console.log(coisas [4]);
 
